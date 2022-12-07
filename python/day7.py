454e0c383c4ec46c03a42d378d206231ef7cf9ba
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2022-12-06 23:14:11 -0700

    :recycle: More elegant solution for Day 7

--- python/day7.py
+++ python/day7.py
@@ -2,14 +2,14 @@ from util import read_stripped_lines
 
 inp = read_stripped_lines("res/day7.txt", "\n")
 
-DIR = 0
-NAME = 1
-PARENT = 2
-CHILDREN = 3
+NAME = 0
+PARENT = 1
+CHILDREN = 2
+SIZE = 3
 
 root = None
 current = None 
-last_spaces = -1
+nodes = []
 
 for line in inp:
     if line.startswith("$"):
@@ -17,7 +17,8 @@ for line in inp:
         if cmd == "cd":
             dirname = rest[0]
             if root == None:
-                root = (True, dirname, None, [])
+                root = [dirname, None, [], 0]
+                nodes.append(root)
                 current = root
                 continue
             if dirname == "..":
@@ -27,47 +28,22 @@ for line in inp:
     else:
         size, name = line.split(" ")
         if size == "dir":
-            current[CHILDREN].append((True, name, current, []))
+            node = [name, current, [], 0]
+            current[CHILDREN].append(node)
+            nodes.append(node)
         else:
-            current[CHILDREN].append((False, name, current, int(size)))
-        
-def nodes_of_size(root, sz):
-    nodes = []
-    def sum_node(node):
-        if isinstance(node[CHILDREN], int):
-            return node[CHILDREN]
-        else:
-            sm = sum(sum_node(x) for x in node[CHILDREN])
-            if sm < sz:
-                nodes.append((sm, node))
-            return sm
-    sum_node(root)
-    return nodes
-        
-def nodes_gt_size(root, sz):
-    nodes = []
-    def sum_node(node):
-        if isinstance(node[CHILDREN], int):
-            return node[CHILDREN]
-        else:
-            sm = sum(sum_node(x) for x in node[CHILDREN])
-            if sm > sz:
-                nodes.append((sm, node))
-            return sm
-    sum_node(root)
-    return nodes
+            s = int(size)
+            t = current[PARENT]
+            while t is not None:
+                t[SIZE] += s
+                t = t[PARENT]
+            current[SIZE] += s
 
 def sum_node(node):
-    if isinstance(node[CHILDREN], int):
-        return node[CHILDREN]
-    else:
-        return sum(sum_node(x) for x in node[CHILDREN])
+    return node[SIZE]
 
-total = 70_000_000 
-reqd = 30_000_000
 used = sum_node(root)
-current = total - used
-target = reqd - current
+target = 30_000_000 - (70_000_000 - used)
 
-print(sum(a[0] for a in nodes_of_size(root, 100000)))
-print(min(a[0] for a in nodes_gt_size(root, target)))
+print(sum(a[SIZE] for a in filter(lambda node: node[SIZE] < 100000, nodes)))
+print(min(a[SIZE] for a in filter(lambda node: node[SIZE] > target, nodes)))
